1e92576c0e8fd38fd4b2d95178d3355494103496
erik.nyquist <enyquist@tartoof.amr.corp.intel.com>
2016-08-08 11:46:44 -0700

    Change Blink.ino to Blink.cpp

--- examples/Blink/Blink.cpp
+++ examples/Blink/Blink.cpp
@@ -13,7 +13,12 @@
   by Scott Fitzgerald
  */
 
+#include <Arduino.h>
+
 // the setup function runs once when you press reset or power the board
+void setup();
+void loop();
+
 void setup() {
   // initialize digital pin 13 as an output.
   pinMode(13, OUTPUT);
@@ -26,3 +31,4 @@ void loop() {
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);              // wait for a second
 }
+
